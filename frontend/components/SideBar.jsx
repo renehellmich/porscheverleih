a99dc8a4907d0931662d173b157f8c8248cdda70
renehellmich <renehellmich94@gmail.com>
2024-03-18 21:47:51 +0100

    add sidebar

--- frontend/components/SideBar.jsx
+++ frontend/components/SideBar.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+
+    const navigate = useNavigate()
+
+  return (
+    <>
+        <div onClick={() => {navigate('/')}}>Home</div>
+        <div onClick={() => {navigate('/buchungen')}}>Buchungen</div>
+        <div onClick={() => {navigate('/autos')}}>Autos</div>
+    </>
+  )
 };
 
 export default SideBar;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,23 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import BuchungForm from '../pages/BuchungForm';
-import NewCar from '../pages/newCar';
+import NewCar from '../pages/NewCar';
 import LandingPage from '../pages/LandingPage';
+import SideBar from "../components/SideBar";
+import Cars from "../pages/Cars";
 
 function App() {
 
   return (
     <>
+    <aside>
+      <SideBar />
+    </aside>
       <Routes>
         <Route path="/" element={<LandingPage />} /> {/* Startseite */}
         <Route path="/buchungen" element={<BuchungForm />} /> {/* Buchungen */}
         <Route path="/buchungen/:id" element={""} /> {/* Buchunge Details */}
-        <Route path="/autos" element={""} /> {/* Autos */}
+        <Route path="/autos" element={<Cars />} /> {/* Autos */}
         <Route path="/autos/:id" element={""} /> {/* Auto Details */}
         <Route path='/autos/neu' element={<NewCar />} /> {/* Auto Details */}
       </Routes>
