bb6931be3f6b411dc5b3e80cc16bb1edc3ad02ef
maylique <maylique14@gmail.com>
2024-03-19 09:15:43 +0100

    fix

--- backend/controller/fahrzeuge.js
+++ backend/controller/fahrzeuge.js
@@ -5,9 +5,4 @@ import { Fahrzeug } from "../models/fahrzeug.js";
 const router = express.Router();
 const mult = multer();
 
-router.get("/fahrzeug", (req, res) => {
-  const Fahrzeuge = Fahrzeug.find();
-  res.send(Fahrzeuge);
-});
-
 export default router;
