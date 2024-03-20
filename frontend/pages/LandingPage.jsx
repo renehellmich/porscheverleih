21b9e5b001030c76312a0fc3fbdc8011b43a1765
renehellmich <renehellmich94@gmail.com>
2024-03-20 11:54:57 +0100

    solve problems by css

--- frontend/pages/LandingPage.jsx
+++ frontend/pages/LandingPage.jsx
@@ -7,32 +7,14 @@ const LandingPage = () => {
 
   return (
     <>
-      <section>
-        <div className="startContainer"
-          style={{
-            width: '64px',
-            height: '64px',
-            background: '#7bff2'
-          }}
-        >
+      <section className="landingContainer">
+        <div className="startContainer">
           Anzahl Buchungen: {buchungen.data.length}
         </div>
-        <div className="startContainer"
-          style={{
-            width: '64px',
-            height: '64px',
-            background: '#7bff2'
-          }}
-        >
+        <div className="startContainer">
           { }
         </div>
-        <div className="startContainer"
-          style={{
-            width: '64px',
-            height: '64px',
-            backgroundColor: '#7bff2'
-          }}
-        >
+        <div className="startContainer">
           Anzahl Autos: {cars.length}
         </div>
       </section>
